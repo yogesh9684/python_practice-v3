52a670571b4a53f97c95fd2c16c4966f5049ae0e
yogesh9684 <yogesh50008@gmail.com>
2023-08-17 18:31:41 +0530

    added another way to swap the numbers

--- Swapping_Numbers.py
+++ Swapping_Numbers.py
@@ -1,9 +1,14 @@
-num1 = 200
-num2 = 400
+# num1 = 200
+# num2 = 400
 
-temp = num1
-num1 = num2
-num2 = temp
+num1 = int(input("Enter the Num1 of your choice="))
+num2 = int(input("Enter the Num2 of your choice="))
+
+# temp = num1
+# num1 = num2
+# num2 = temp
+
+num1 ,num2 = num2 , num1
 
 print("Number Num1 After Swapping",num1)
 print("Number Num2 After Swapping",num2)
