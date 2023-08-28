f0f591b8fcdb1ecbbb029fa7b509c6c780f93b3d
yogesh9684 <yogesh50008@gmail.com>
2023-08-28 15:47:25 +0530

    Count of Element in List

---
+++ Finding_Length_Of_List.py
@@ -0,0 +1,14 @@
+#Approch1
+
+List1= [1,4,3,2,6,9,4,512,43]
+print(len(List1))
+
+#Approch 2
+
+List1= [1,4,3,2,6,9,4,512,43]
+count = 0
+
+for i in List1:
+    count = count +1
+print("Length of List",count)
+
