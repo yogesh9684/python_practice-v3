5332dfe76cdf18edaf40127b051367906e8d4da7
yogesh9684 <yogesh50008@gmail.com>
2023-08-28 17:50:48 +0530

    Clearing the List

---
+++ Clearing_a_List.py
@@ -0,0 +1,22 @@
+#Approch 1
+
+List1 = [1,2,3,4,5,6,7,8,9]
+List1.clear()
+print(List1)
+
+# Approch 2
+
+List1 = [1,2,3,4,5,6,7,8,9]
+del List1 [::]
+print(List1)
+
+#Approch 3
+
+List1 = [1,2,3,4,5,6,7,8,9]
+List1=[]
+print(List1)
+
+#Approch 4
+List1 = [1,2,3,4,5,6,7,8,9]
+List1 *= 0
+print(List1)
