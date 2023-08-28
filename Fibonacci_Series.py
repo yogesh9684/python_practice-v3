1281ac7e0bf55bcfe7f8b6ee3a1d65a3b2373a15
yogesh9684 <yogesh50008@gmail.com>
2023-08-28 14:19:45 +0530

    Fibonacci Series

--- Fibonacci_Series.py
+++ Fibonacci_Series.py
@@ -1,14 +1,14 @@
 # Fibonacci Series is the Addition of Previous two Numbers
 # initial Default Value 0 and 1
 
-Num1= 0
-Num2= 1
-print(Num1)
-print(Num2)
+num1= 0
+num2= 1
+print(num1)
+print(num2)
 
 for i in range(1 , 10 ):
-    sum = Num1 + Num2
+    sum = num1 + num2
     print(sum)
-    Num1 = Num2
-    Num2 = sum
-    sum = Num1
+    num1 = num2
+    num2 = sum
+    sum = num1
