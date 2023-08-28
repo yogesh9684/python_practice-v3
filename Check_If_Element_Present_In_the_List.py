dd2f75c057290aa08f8a9d7ab5f0d670fab5480e
yogesh9684 <yogesh50008@gmail.com>
2023-08-28 16:43:21 +0530

    Check Element is Present in List

---
+++ Check_If_Element_Present_In_the_List.py
@@ -0,0 +1,20 @@
+# Approch 1
+
+List1 = [1,2,3,4,5,6,7,8,9]
+for i in List1:
+    if i == 11:
+        print("Element Present")
+        break
+else:
+    print("Element Not Present")
+
+#Approch 2
+
+List1 = [1,2,3,4,5,6,7,8,9]
+Element = 9
+if Element in List1:
+    print("Element Available")
+else:
+    print("Element Not Present")
+
+
