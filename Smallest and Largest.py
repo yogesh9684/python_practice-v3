322ff2c4193405ed554b3dc06c3ff6967afbe84a
yogesh9684 <yogesh50008@gmail.com>
2023-09-05 22:06:51 +0530

    finding Smallest and Largest Number from List

---
+++ Smallest and Largest.py
@@ -0,0 +1,11 @@
+#Approch1
+# List1 = [1,200,33,444,56,16,57,88]
+# print(min(List1))
+# print(max(List1))
+
+#Approch2
+List1 = [1,200,33,444,56,16,57,88]
+List1.sort()
+print(List1)
+print(List1[0])
+print(List1[-1])
