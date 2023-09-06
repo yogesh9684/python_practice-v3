49c2b0d09f032dd9f8328ae1a1a66838a09a1989
yogesh9684 <yogesh50008@gmail.com>
2023-09-06 11:16:38 +0530

    Verify Length of String

---
+++ Length_of_String.py
@@ -0,0 +1,13 @@
+#Approch 1
+
+String = "HELLOWORLD"
+Count = 0
+
+for i in String:
+    Count = Count + 1
+print("Length of String is",Count)
+
+#Approch 2
+
+String = "HELLOWORLD"
+print(len(String))
