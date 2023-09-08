c69489ef729898e203105e427b21b934b05ef1ea
yogesh9684 <yogesh50008@gmail.com>
2023-09-08 12:28:01 +0530

    Return Statement

---
+++ Return Value.py
@@ -0,0 +1,12 @@
+# Directly printing the Values
+def add(n1 , n2):
+    print(n1+n2)
+
+sum = add(10,8)
+
+# Working with Returning Statement
+def add(n1, n2):
+    return(n1 + n2)
+
+sum = add(10, 10)
+print(sum)
