1eb14fcd0717db752c354322d4eb0c76e267a4e9
yogesh9684 <yogesh50008@gmail.com>
2023-09-10 00:20:38 +0530

    Working with files

---
+++ Working_with_files.py
@@ -0,0 +1,20 @@
+
+# How to WRITE to A File
+
+# List = ["Yogesh ","Dipakrao ","Sawant"]
+# MyFile =open("E:\workspace_python\Basic_Programs\sample_working.txt","w")
+# for item in List:
+#     MyFile.write(str(item))
+# print("File has been written")
+# MyFile.close()
+
+# How to READ a File
+
+# MyFile = open("E:\workspace_python\Basic_Programs\sample_working.txt",'r')
+# print(str(MyFile.read()))
+# MyFile.close()
+
+# It will Just read the Single line of the File
+MyFile = open("E:\workspace_python\Basic_Programs\sample_working.txt",'r')
+print(str(MyFile.readline()))
+MyFile.close()
